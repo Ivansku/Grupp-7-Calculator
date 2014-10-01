6f4108989b599a336b62b70cd9338562873e7f9f
Xpoq <j.890324@gmail.com>
2014-10-01 21:43:11 +0200

    Updated Median

--- Calc.py
+++ Calc.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 def mathPlus(a, b):
     
     return a + b
@@ -14,15 +15,33 @@ def mathDiv(a, b):
 
     return a / b
 
-def mathAvg (a, b):
+def mathAvg(a, b):
 
     return (a + b)/2
 
-def mathRaised (a, b):
+def mathRaised(a, b):
     return a**b
 
-def mathMed (a, b, c):
-
-    sorted([a, b, c])
-
-    return b
+#variabler för median värdet om större eller mindre
+def bigger(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+def smaller (a, b):
+    if a < b:
+        return a
+    else:
+        return b
+    
+#själva inputen för medianen
+def mathMed(a, b, c):
+    if a > bigger(b, c):
+        return bigger(b, c)
+    else:
+        if a < smaller(b, c):
+            return smaller(b, c)
+        else:
+            return a
+        if a == b or c:
+            return a
